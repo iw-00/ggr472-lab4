f72b41b92b1c26c6a0226f834503ea780563fed3
iw-00 <133152066+iw-00@users.noreply.github.com>
2025-03-12 16:52:23 -0400

    Update script.js
    create bounding box

--- script.js
+++ script.js
@@ -24,17 +24,98 @@ Step 2: VIEW GEOJSON POINT DATA ON MAP
 //      Use the fetch method to access the GeoJSON from your online repository
 //      Convert the response to JSON format and then store the response in your new variable
 
+let collisiongeojson;
 
+// Fetch geojson from url and store response.
+fetch("https://raw.githubusercontent.com/iw-00/ggr472-lab4/refs/heads/main/data/pedcyc_collision_06-21.geojson")
+    .then(response => response.json())
+    .then(response => {
+        console.log(response); //Check response in console
+        collisiongeojson = response; // Store geojson as variable using URL from fetch response
+    });
 
-/*--------------------------------------------------------------------
+// Check points
+map.on('load', () => {
+
+    //Add datasource using GeoJSON variable
+    map.addSource('toronto-collision', {
+        type: 'geojson',
+        data: collisiongeojson
+    });
+
+    map.addLayer({
+        'id': 'toronto-collision-pts',
+        'type': 'circle',
+        'source': 'toronto-collision',
+        'paint': {
+            'circle-radius': 5,
+            'circle-color': 'blue'
+        }
+    });
+
+    /*--------------------------------------------------------------------
     Step 3: CREATE BOUNDING BOX AND HEXGRID
---------------------------------------------------------------------*/
-//HINT: All code to create and view the hexgrid will go inside a map load event handler
-//      First create a bounding box around the collision point data
-//      Access and store the bounding box coordinates as an array variable
-//      Use bounding box coordinates as argument in the turf hexgrid function
-//      **Option: You may want to consider how to increase the size of your bbox to enable greater geog coverage of your hexgrid
-//                Consider return types from different turf functions and required argument types carefully here
+    --------------------------------------------------------------------*/
+    //HINT: All code to create and view the hexgrid will go inside a map load event handler
+    //      First create a bounding box around the collision point data
+    //      Access and store the bounding box coordinates as an array variable
+    //      Use bounding box coordinates as argument in the turf hexgrid function
+    //      **Option: You may want to consider how to increase the size of your bbox to enable greater geog coverage of your hexgrid
+    //      Consider return types from different turf functions and required argument types carefully here
+
+    // Create a bounding box from the collision data.
+    let envresult = turf.envelope(collisionData);
+
+    // Increase size of bounding box.
+    let bboxscaled = turf.transformScale(envresult, 1.1);
+    console.log(bboxscaled)
+
+    // Get min and max coords of bounding box axes.
+    let bboxcoords = [
+        bboxscaled.geometry.coordinates[0][0][0],
+        bboxscaled.geometry.coordinates[0][0][1],
+        bboxscaled.geometry.coordinates[0][2][0],
+        bboxscaled.geometry.coordinates[0][2][1],
+    ];
+
+    let hexdata = turf.hexGrid(bboxcoords, 0.5, {units: "kilometers"});
+
+    let collishex = turf.collect(hexdata, collisionData, "_id", "values");
+
+    // Aggregate collision data by hexgrid.
+    let maxcollisions = 0;
+
+    collishex.features.forEach((feature) => {
+        feature.properties.COUNT = feature.properties.values.length;
+        if (feature.properties.COUNT > maxcollisions) {
+            maxcollisions = feature.properties.COUNT
+        }
+    });
+
+    map.addSource("collishexgrid", {
+        type: "geojson",
+        data: collishex,
+    });
+
+    map.addLayer({
+        id: "collishexfill",
+        type: "fill",
+        source: "collishexgrid",
+        paint: {
+            "fill-color": [
+                "step",
+                ["get", "COUNT"],
+                "#ffffff",
+                10, "#ffebf1",
+                25, "#ffa0a6",
+                maxcollisions, "#ff0000"
+            ],
+            "fill-opacity": 0.8
+        },
+        filter: ["!=", "COUNT", 0],
+    });
+
+});
 
 
 
