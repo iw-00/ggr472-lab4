37b0b1d7f4af84f57bbaa8029c16899b15c9f443
ywiris0 <irisy.wu@mail.utoronto.ca>
2025-03-13 15:39:18 -0400

    update script
    add hexgrid, interactivity

--- script.js
+++ script.js
@@ -11,9 +11,10 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiaXcwMCIsImEiOiJjbTV2aXFlajYwMjZmMmtvbWtrMGRhd
 // Initialize map and edit to your preference
 const map = new mapboxgl.Map({
     container: 'map', // container id in HTML
-    style: '',  // ****ADD MAP STYLE HERE *****
-    center: [-79.39, 43.65],  // starting point, longitude/latitude
-    zoom: 11 // starting zoom level
+    style: 'mapbox://styles/iw00/cm7v0wnd701tl01qohx5y9nop',  // ****ADD MAP STYLE HERE *****
+    // style: "mapbox://styles/iw00/cm87oo607000p01qr2nbl6kn3",
+    center: [-79.372, 43.736],  // starting point, longitude/latitude
+    zoom: 10 // starting zoom level
 });
 
 
@@ -30,27 +31,16 @@ let collisiongeojson;
 fetch("https://raw.githubusercontent.com/iw-00/ggr472-lab4/refs/heads/main/data/pedcyc_collision_06-21.geojson")
     .then(response => response.json())
     .then(response => {
-        console.log(response); //Check response in console
+        // console.log(response); //Check response in console
         collisiongeojson = response; // Store geojson as variable using URL from fetch response
     });
 
-// Check points
 map.on('load', () => {
 
-    //Add datasource using GeoJSON variable
+    // Add collision datasource using GeoJSON variable
     map.addSource('toronto-collision', {
         type: 'geojson',
         data: collisiongeojson
-    });
-
-    map.addLayer({
-        'id': 'toronto-collision-pts',
-        'type': 'circle',
-        'source': 'toronto-collision',
-        'paint': {
-            'circle-radius': 5,
-            'circle-color': 'blue'
-        }
     });
 
     /*--------------------------------------------------------------------
@@ -64,11 +54,12 @@ map.on('load', () => {
     //      Consider return types from different turf functions and required argument types carefully here
 
     // Create a bounding box from the collision data.
-    let envresult = turf.envelope(collisionData);
+    let envresult = turf.envelope(collisiongeojson);
+    // console.log(envresult);
 
     // Increase size of bounding box.
     let bboxscaled = turf.transformScale(envresult, 1.1);
-    console.log(bboxscaled)
+    // console.log(bboxscaled);
 
     // Get min and max coords of bounding box axes.
     let bboxcoords = [
@@ -79,24 +70,52 @@ map.on('load', () => {
     ];
 
     let hexdata = turf.hexGrid(bboxcoords, 0.5, {units: "kilometers"});
-
-    let collishex = turf.collect(hexdata, collisionData, "_id", "values");
+    // console.log(hexdata); // check in console
+ 
+    /*--------------------------------------------------------------------
+    Step 4: AGGREGATE COLLISIONS BY HEXGRID
+    --------------------------------------------------------------------*/
+    //HINT: Use Turf collect function to collect all '_id' properties from the collision points data for each heaxagon
+    //      View the collect output in the console. Where there are no intersecting points in polygons, arrays will be empty
 
     // Aggregate collision data by hexgrid.
-    let maxcollisions = 0;
+    let collishex = turf.collect(hexdata, collisiongeojson, "_id", "values");
+    console.log(collishex.features); // check in console
+
+    // Add point count and identify max number of collisions in a polygon
+    let maxcollisions = 0; // initialize max collisions as 0
 
     collishex.features.forEach((feature) => {
-        feature.properties.COUNT = feature.properties.values.length;
+        feature.properties.COUNT = feature.properties.values.length; // set COUNT to number of points in polygon
         if (feature.properties.COUNT > maxcollisions) {
-            maxcollisions = feature.properties.COUNT
+            maxcollisions = feature.properties.COUNT // if polygon COUNT is greater than current max, set COUNT to max.
         }
     });
 
+    // Add hexgrid source.
+    map.addSource("hexdata", {
+        type: "geojson",
+        data: hexdata,
+    });
+
+    // Add aggregated collision hexgrid source.
     map.addSource("collishexgrid", {
         type: "geojson",
         data: collishex,
     });
 
+    // Check hexgrid by adding to map
+    // map.addLayer({
+    //     id: "hexgrid",
+    //     type: "fill",
+    //     source: "hexdata",
+    //     paint: {
+    //         "fill-color": "blue",
+    //         "fill-opacity": 0.5
+    //     }
+    // });
+
+    // Add collision hexgrid to map.
     map.addLayer({
         id: "collishexfill",
         type: "fill",
@@ -105,27 +124,45 @@ map.on('load', () => {
             "fill-color": [
                 "step",
                 ["get", "COUNT"],
-                "#ffffff",
-                10, "#ffebf1",
-                25, "#ffa0a6",
-                maxcollisions, "#ff0000"
+                "#fef0d9",
+                10, "#fdcc8a",
+                25, "#fc8d59",
+                maxcollisions, "#d7301f"
             ],
-            "fill-opacity": 0.8
+            'fill-opacity': [
+                'case',
+                ['boolean', ['feature-state', 'hover'], false],
+                1,
+                0.8
+            ],
         },
-        filter: ["!=", "COUNT", 0],
+        filter: ["!=", "COUNT", 0], // filter out hexagons with 0 collisions
     });
 
+    // Check points
+    map.addLayer({
+        'id': 'toronto-collision-pts',
+        'type': 'circle',
+        'source': 'toronto-collision',
+        "layout": {
+            "visibility": "none"
+        },
+        'paint': {
+            'circle-radius': 3,
+            'circle-color': '#a62c14',
+            "circle-opacity": 0.8,
+            "circle-color": [
+                "match",
+                ["get", "INVTYPE"],
+                "Pedestrian", // Right-of-Way
+                "#1b9e77",
+                "Cyclist", // Open Space Site (Park)
+                "#e7298a",
+                "#7570b3" // Other
+            ],
+        }
+    });
 });
-
-
-
-/*--------------------------------------------------------------------
-Step 4: AGGREGATE COLLISIONS BY HEXGRID
---------------------------------------------------------------------*/
-//HINT: Use Turf collect function to collect all '_id' properties from the collision points data for each heaxagon
-//      View the collect output in the console. Where there are no intersecting points in polygons, arrays will be empty
-
-
 
 // /*--------------------------------------------------------------------
 // Step 5: FINALIZE YOUR WEB MAP
@@ -137,4 +174,70 @@ Step 4: AGGREGATE COLLISIONS BY HEXGRID
 //        - The maximum number of collisions found in a hexagon
 //      Add a legend and additional functionality including pop-up windows
 
+document.getElementById('ptlayercheck').addEventListener('change', (e) => {
+    map.setLayoutProperty(
+        'toronto-collision-pts',
+        'visibility',
+        e.target.checked ? 'visible' : 'none'
+    );
+});
 
+document.getElementById('hexlayercheck').addEventListener('change', (e) => {
+    map.setLayoutProperty(
+        'collishexfill',
+        'visibility',
+        e.target.checked ? 'visible' : 'none'
+    );
+});
+
+/*--------------------------------------------------------------------
+ADD POP-UP ON CLICK EVENT
+--------------------------------------------------------------------*/
+map.on('mouseenter', 'collishexfill', () => {
+    map.getCanvas().style.cursor = 'pointer'; //Switch cursor to pointer when mouse is over provterr-fill layer
+});
+
+map.on('mouseleave', 'collishexfill', () => {
+    map.getCanvas().style.cursor = ''; //Switch cursor back when mouse leaves provterr-fill layer
+});
+
+map.on('click', 'collishexfill', (e) => {
+    new mapboxgl.Popup() //Declare new popup object on each click
+        .setLngLat(e.lngLat) //Use method to set coordinates of popup based on mouse click location
+        .setHTML("<b>Number of collisions:</b> " + e.features[0].properties.COUNT + "<br>" +
+            "Population: " + e.features[0].properties.POP2021) //Use click event properties to write text for popup
+        .addTo(map); //Show popup on map
+})
+
+/*--------------------------------------------------------------------
+HOVER EVENT
+// --------------------------------------------------------------------*/
+let hexID = null; //Declare initial province ID as null
+
+map.on('mousemove', 'collishexfill', (e) => {
+    if (e.features.length > 0) { //If there are features in array enter conditional
+
+        if (hexID !== null) { //If hexID IS NOT NULL set hover feature state back to false to remove opacity from previous highlighted polygon
+            map.setFeatureState(
+                { source: 'collishexgrid', id: hexID },
+                { hover: false }
+            );
+        }
+
+        hexID = e.features[0].id; //Update hexID to featureID
+        map.setFeatureState(
+            { source: 'collishexgrid', id: hexID },
+            { hover: true } //Update hover feature state to TRUE to change opacity of layer to 1
+        );
+    }
+});
+
+map.on('mouseleave', 'collishexfill', () => { //If mouse leaves the geojson layer, set all hover states to false and hexID variable back to null
+    if (hexID !== null) {
+        map.setFeatureState(
+            { source: 'collishexgrid', id: hexID },
+            { hover: false }
+        );
+    }
+    hexID = null;
+});
